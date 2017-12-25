65fe07fde0b22e9993f119d54b297b8a2c5dfa78
maker9 <xpninja@me.com>
2017-12-25 11:01:38 +0800

    修改PID文件

--- PID.py
+++ PID.py
@@ -1,17 +1,4 @@
-#The recipe gives simple implementation of a Discrete Proportional-Integral-Derivative (PID) controller. PID controller gives output value for error between desired reference input and measurement feedback to minimize error value.
 #More information: http://en.wikipedia.org/wiki/PID_controller
-#
-#cnr437@gmail.com
-#
-#######	Example	#########
-#
-#p=PID(3.0,0.4,1.2)
-#p.setPoint(5.0)
-#while True:
-#     pid = p.update(measurement_value)
-#
-#
-
 
 class PID:
 	"""
@@ -20,18 +7,18 @@ class PID:
 
 	def __init__(self, P=2.0, I=0.0, D=1.0, Derivator=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
 
-		self.Kp=P
-		self.Ki=I
-		self.Kd=D
-		self.Derivator=Derivator
-		self.Integrator=Integrator
-		self.Integrator_max=Integrator_max
-		self.Integrator_min=Integrator_min
+		self.Kp = P
+		self.Ki = I
+		self.Kd = D
+		self.Derivator = Derivator
+		self.Integrator = Integrator
+		self.Integrator_max = Integrator_max
+		self.Integrator_min = Integrator_min
 
-		self.set_point=0.0
-		self.error=0.0
+		self.set_point = 0.0
+		self.error = 0.0
 
-	def update(self,current_value):
+	def update(self, current_value):
 		"""
 		Calculate PID output value for given reference input and feedback
 		"""
@@ -39,7 +26,7 @@ class PID:
 		self.error = self.set_point - current_value
 
 		self.P_value = self.Kp * self.error
-		self.D_value = self.Kd * ( self.error - self.Derivator)
+		self.D_value = self.Kd * (self.error - self.Derivator)
 		self.Derivator = self.error
 
 		self.Integrator = self.Integrator + self.error
@@ -55,13 +42,13 @@ class PID:
 
 		return PID
 
-	def setPoint(self,set_point):
+	def setPoint(self, set_point):
 		"""
 		Initilize the setpoint of PID
 		"""
 		self.set_point = set_point
-		self.Integrator=0
-		self.Derivator=0
+		self.Integrator = 0
+		self.Derivator = 0
 
 	def setIntegrator(self, Integrator):
 		self.Integrator = Integrator
@@ -69,14 +56,14 @@ class PID:
 	def setDerivator(self, Derivator):
 		self.Derivator = Derivator
 
-	def setKp(self,P):
-		self.Kp=P
+	def setKp(self, P):
+		self.Kp = P
 
-	def setKi(self,I):
-		self.Ki=I
+	def setKi(self, I):
+		self.Ki = I
 
-	def setKd(self,D):
-		self.Kd=D
+	def setKd(self, D):
+		self.Kd = D
 
 	def getPoint(self):
 		return self.set_point
